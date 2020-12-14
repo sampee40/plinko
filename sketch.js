2287367fbb0317da18b90d4d784e29f77ad82c75
sampee40 <67319419+sampee40@users.noreply.github.com>
2020-12-14 20:03:38 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+function setup() {
+  createCanvas(480,800);
+
+  var divisions1 = createSprite(70, 800, 10, 800);
+divisions1.shapecolor=("white");
+var divisions2 = createSprite(140, 800, 10, 800);
+divisions2.shapecolor=("white");
+var divisions3 = createSprite(210, 800, 10, 800);
+divisions3.shapecolor=("white");
+var divisions4 = createSprite(280, 800, 10, 800);
+divisions4.shapecolor=("white");
+var divisions5 = createSprite(350, 800, 10, 800);
+divisions5.shapecolor=("white");
+var divisions6 = createSprite(420, 800, 10, 800);
+divisions6.shapecolor=("white");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+function draw() {
+  background("black");  
+  drawSprites();
+}
